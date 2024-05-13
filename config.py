870c22506792af5534239501a2f8c0daeb64e4a5
Piklu Dey <pikludey1990@gmail.com>
2024-05-13 23:52:03 +0530

    update api key

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 config = {
-    "google_api_key": "AIzaSyCDh7qAGegt0wjACkUut4-KX0jQM3lv1nw",
+    "google_api_key": "AIzaSyCDh7qAGegt0wjACkUut4-KX0jQM3lv1nwQQa145",
     "youtube_playlist_id": "PL5dTjWUk_cPYztKD7WxVFluHvpBNM28N9"
 }
